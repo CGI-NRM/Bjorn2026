ebb97cd50fcc27df14af8ee22a4f1a94e500da47
wfenglund <w.f.englund@gmail.com>
2026-08-04 12:39:00 +0200

    changed map name from karta to Karta to work on iphones as well

--- auto_update.sh
+++ auto_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 R CMD BATCH regen.R
-sed -i -e '/^<html xmlns="http:\/\/www.w3.org\/1999\/xhtml">$/r matomo.js' karta.html
-git add karta.html
+sed -i -e '/^<html xmlns="http:\/\/www.w3.org\/1999\/xhtml">$/r matomo.js' Karta.html
+git add Karta.html
 git commit -m "Update map"
 git push
